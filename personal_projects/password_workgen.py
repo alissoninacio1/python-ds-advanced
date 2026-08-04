c9d177f38675b286a7d5c07256447b614e05e15e
alissoninacio1 <alissoninacio96@gmail.com>
2026-08-04 13:44:42 -0600

    Done

--- personal_projects/password_workgen.py
+++ personal_projects/password_workgen.py
@@ -7,13 +7,15 @@ from datetime import datetime as dt
 def pass_gen(site: str):
     formatted = dt.now().strftime("%H%M")
 
-    resumed_name = site.lower().replace(" ", "")[:3]
-    adjusted_name = resumed_name[0].upper() + resumed_name[1:3]
+    # resumed_name = site.lower().replace(" ", "")[:3]
+    # adjusted_name = resumed_name[0].upper() + resumed_name[1:3]
 
-    #If your goal is simply to turn a string like " john doe " into "Joh", Python has a built-in .capitalize() method:python# Removes spaces, grabs first 3 letters, capitalizes index 0
+    #If your goal is simply to turn a string like " john doe " into "Joh", Python has a built-in .capitalize() method:python
+    #Removes spaces, grabs first 3 letters, capitalizes index 0
     #adjusted_name = name.replace(" ", "")[:3].capitalize()
 
-    print(f"{site}{formatted}")
+    adjusted_name = site.strip()[:3].capitalize()
+    print(f"{adjusted_name}.{formatted}")
 
 
 def main():
